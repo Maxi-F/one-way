fdeddc2c4aa0f79edad5bbd5e8f00ca2b0b4c5f8
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 04:04:27 -0300

    Add notes sounds

--- Assets/ScriptableObjects/Scripts/JumpCoinConfig.cs
+++ Assets/ScriptableObjects/Scripts/JumpCoinConfig.cs
@@ -9,6 +9,8 @@ namespace ScriptableObjects.Scripts
         public List<GameObject> noteObjects = new List<GameObject>();
         public List<Material> materialsList = new List<Material>();
 
+        [Header("Sounds")] public List<string> sounds = new List<string>();
+        
         [Header("Hover settings")]
         public float hoverVelocity = 0.5f;
         public float hoverDistance = 0.2f;
--- Assets/Scripts/Coins/JumpCoin/JumpCoin.cs
+++ Assets/Scripts/Coins/JumpCoin/JumpCoin.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
+using Audio;
 using Manager;
 using PlayerScripts;
 using ScriptableObjects.Scripts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Coins.JumpCoin
 {
@@ -10,14 +13,18 @@ namespace Coins.JumpCoin
     {
         [Header("Coin config")]
         [SerializeField] private JumpCoinConfig config;
-    
+
+        private string _noteSound;
+        
         public void Start()
         {
             RemoveDebug();
         
             JumpCoinFactory factory = new JumpCoinFactory(config, FindObjectOfType<Player>());
-
+            
             factory.CreateJumpCoin(gameObject);
+
+            _noteSound = config.sounds[Random.Range(0, config.sounds.Count - 1)];
         }
 
         public void OnTriggerEnter(Collider other)
@@ -26,6 +33,8 @@ namespace Coins.JumpCoin
             {
                 other.gameObject.GetComponent<PlayerController>().AddJump();
 
+                AudioManager.Instance.PlaySound(_noteSound);
+                
                 EventManager.Instance.TriggerEvent(
                     "coinObtained",
                     new Dictionary<string, object>()
