b102d8cea460a3c4cfb53e15d3f458962cc35f3d
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 18:00:37 -0300

    Refactor Camera scripts

--- Assets/Scripts/Camera/CameraBehaviour.cs
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -12,7 +12,10 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField] private float followSpeed = 5;
 
     [Header("Rotation properties")] 
+    [Tooltip("Max Angles on the top of the target in which the camera can rotate in X")] 
     [SerializeField] private Vector2 topMinMaxAngles = new Vector2(80.0f, 100.0f);
+    
+    [Tooltip("Max Angles on the bottom of the target in which the camera can rotate in X")] 
     [SerializeField] private Vector2 bottomMinMaxAngles = new Vector2(260.0f, 280.0f);
     
     private Vector2 _desiredRotation;
@@ -28,6 +31,12 @@ public class CameraBehaviour : MonoBehaviour
         ModifyRotation();
     }
 
+    /// <summary>
+    /// Rotates the camera around the target setted as a serialized field,
+    /// Taking into account if the rotation was made with controller or mouse.
+    ///
+    /// It also takes into account top and bottom max angles, so it doesn't do a 360 rotation in X.
+    /// </summary>
     private void ModifyRotation()
     {
         Quaternion previousRotationInX = transform.rotation;
@@ -47,7 +56,11 @@ public class CameraBehaviour : MonoBehaviour
         
         transform.RotateAround(target.position, Vector3.up, _desiredRotation.x * player.Sensibility * multiplier);
     }
-
+    
+    /// <summary>
+    /// Modifies the position of the camera, taking into account the offset
+    /// from the target. 
+    /// </summary>
     private void ModifyPosition()
     {
         var rotatedOffset = transform.rotation * offset;
@@ -56,6 +69,11 @@ public class CameraBehaviour : MonoBehaviour
         transform.position = Vector3.Slerp(transform.position, offsetEmulatingTransformPoint, Time.fixedDeltaTime * followSpeed);
     }
 
+    /// <summary>
+    /// Sets the rotation desired direction.
+    /// </summary>
+    /// <param name="lookInput">Vector2 input of the controller/mouse</param>
+    /// <param name="isController">flag to check if the input provided is from mouse or controller.</param>
     public void RotateCamera(Vector2 lookInput, bool isController)
     {
         _isController = isController;
