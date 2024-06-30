3a3292b07c4cfc9d9c8add471f3028af89e48eca
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 19:34:10 -0300

    Refactor menu handler

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,22 +1,25 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using Manager;
 using UnityEngine;
 
-public class MenuHandler : MonoBehaviour
+namespace Menu
 {
-    [SerializeField] private string menuName;
-    [SerializeField] private string menuActivatedEvent = "menuActivated";
-    [SerializeField] private string menuDeactivatedEvent = "menuDeactivated";
+    public class MenuHandler : MonoBehaviour
+    {
+        [SerializeField] private string menuName;
     
-    void Start()
-    {
-        EventManager.Instance?.TriggerEvent(menuActivatedEvent, new Dictionary<string, object>() { { "name", menuName } });
-    }
+        [Header("Events")]
+        [SerializeField] private string menuActivatedEvent = "menuActivated";
+        [SerializeField] private string menuDeactivatedEvent = "menuDeactivated";
+    
+        void Start()
+        {
+            EventManager.Instance?.TriggerEvent(menuActivatedEvent, new Dictionary<string, object>() { { "name", menuName } });
+        }
 
-    private void OnDisable()
-    {
-        EventManager.Instance?.TriggerEvent(menuDeactivatedEvent, new Dictionary<string, object>() { { "name", menuName } });
+        private void OnDisable()
+        {
+            EventManager.Instance?.TriggerEvent(menuDeactivatedEvent, new Dictionary<string, object>() { { "name", menuName } });
+        }
     }
 }
