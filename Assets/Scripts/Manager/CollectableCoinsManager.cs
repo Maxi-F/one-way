2d79d210151995e646fe94d9633b306e374fa993
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 19:01:23 -0300

    Refactor collectable coins manager

--- Assets/Scripts/Manager/CollectableCoinsManager.cs
+++ Assets/Scripts/Manager/CollectableCoinsManager.cs
@@ -1,72 +1,63 @@
-using System;
-using PlayerScripts;
-using System.Collections;
 using System.Collections.Generic;
 using Audio;
 using Coins;
 using Coins.CollectableCoin;
-using Manager;
+using PlayerScripts;
 using UnityEngine;
 
-public class CollectableCoinsManager : MonoBehaviour
+namespace Manager
 {
-    [SerializeField] private Player player;
+    public class CollectableCoinsManager : MonoBehaviour
+    {
+        [SerializeField] private Player player;
 
-    [Header("Sounds")] 
-    [SerializeField] private string obtainStarSound = "star";
-    [SerializeField] private string allStarsCollectedSound = "allStarsCollected";
+        [Header("Sounds")] 
+        [SerializeField] private string obtainStarSound = "star";
+        [SerializeField] private string allStarsCollectedSound = "allStarsCollected";
     
-    [Header("Events")]
-    [SerializeField] private string collectableCoinObtainedEvent = "collectableCoinObtained";
-    [SerializeField] private string playerLostEvent = "playerLost";
-    [SerializeField] private string allCoinsCollectedEvent = "allCoinsCollected";
-    [SerializeField] private string collectableCoinsCountEvent = "collectableCoinsCount";
+        [Header("Events")]
+        [SerializeField] private string collectableCoinObtainedEvent = "collectableCoinObtained";
+        [SerializeField] private string allCoinsCollectedEvent = "allCoinsCollected";
+        [SerializeField] private string collectableCoinsCountEvent = "collectableCoinsCount";
     
-    private int _coinsObtained = 0;
-    private CollectableCoin[] _collectableCoins;
-    void Start()
-    {
-        _collectableCoins = FindObjectsOfType<CollectableCoin>();
+        private int _coinsObtained = 0;
+        private CollectableCoin[] _collectableCoins;
+        void Start()
+        {
+            _collectableCoins = FindObjectsOfType<CollectableCoin>();
         
-        foreach (var collectableCoin in _collectableCoins)
-        {
-            CoinLookAt coinLookAt = collectableCoin.GetComponentInChildren<CoinLookAt>();
-            coinLookAt.SetTransform(player.transform);
-        }
+            foreach (var collectableCoin in _collectableCoins)
+            {
+                CoinLookAt coinLookAt = collectableCoin.GetComponentInChildren<CoinLookAt>();
+                coinLookAt.SetTransform(player.transform);
+            }
         
-        EventManager.Instance?.TriggerEvent(collectableCoinsCountEvent, new Dictionary<string, object>() { { "value", _collectableCoins.Length } });
-        EventManager.Instance?.SubscribeTo(collectableCoinObtainedEvent, OnCoinObtained);
-        EventManager.Instance?.SubscribeTo(playerLostEvent, OnReset);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.Instance?.UnsubscribeTo(collectableCoinObtainedEvent, OnCoinObtained);
-        EventManager.Instance?.UnsubscribeTo(playerLostEvent, OnReset);
-    }
-
-    void OnCoinObtained(Dictionary<string, object> message)
-    {
-        _coinsObtained++;
-
-        if (_coinsObtained == _collectableCoins.Length)
-        {
-            AudioManager.Instance?.PlaySound(allStarsCollectedSound);
-            EventManager.Instance?.TriggerEvent(allCoinsCollectedEvent, null);
-        }
-        else
-        {
-            AudioManager.Instance?.PlaySound(obtainStarSound);
-        }
-    }
-
-    void OnReset(Dictionary<string, object> message)
-    {
-        foreach (var collectableCoin in _collectableCoins)
-        {
-            collectableCoin.Reset();
+            EventManager.Instance?.TriggerEvent(collectableCoinsCountEvent, new Dictionary<string, object>() { { "value", _collectableCoins.Length } });
+            EventManager.Instance?.SubscribeTo(collectableCoinObtainedEvent, OnCoinObtained);
         }
 
-        _coinsObtained = 0;
+        private void OnDisable()
+        {
+            EventManager.Instance?.UnsubscribeTo(collectableCoinObtainedEvent, OnCoinObtained);
+        }
+
+        /// <summary>
+        /// When the event is called, it sums one coin obtained and checks if its the last coin to obtain.
+        /// If it is the last one, it triggers the all coins collected event.
+        /// </summary>
+        void OnCoinObtained(Dictionary<string, object> message)
+        {
+            _coinsObtained++;
+
+            if (_coinsObtained == _collectableCoins.Length)
+            {
+                AudioManager.Instance?.PlaySound(allStarsCollectedSound);
+                EventManager.Instance?.TriggerEvent(allCoinsCollectedEvent, null);
+            }
+            else
+            {
+                AudioManager.Instance?.PlaySound(obtainStarSound);
+            }
+        }
     }
 }
