794297684c08ac61ec1c2afdbe889572428e29d7
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 17:38:10 -0300

    Another Refactor on animations scripts

--- Assets/Scripts/Animations/DoubleJumpExit.cs
+++ Assets/Scripts/Animations/DoubleJumpExit.cs
@@ -4,9 +4,11 @@ namespace Animations
 {
     public class DoubleJumpExit : StateMachineBehaviour
     {
+        private string isDoubleJumpingEvent = "isDoubleJumping";
+        
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            animator.SetBool("isDoubleJumping", false);
+            animator.SetBool(isDoubleJumpingEvent, false);
         }
     }
 }
