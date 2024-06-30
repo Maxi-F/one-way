d1eef3c7d24301502914531d69ca0688b58d5869
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 19:30:32 -0300

    Refactor SceneryManager

--- Assets/Scripts/Manager/SceneryManager.cs
+++ Assets/Scripts/Manager/SceneryManager.cs
@@ -10,12 +10,14 @@ namespace Manager
 {
     public class SceneryManager : MonoBehaviour
     {
-        [FormerlySerializedAs("scenesData")] [SerializeField] private ScenesDataConfig scenesDataConfig;
-        [SerializeField] private string[] initScenes = new string[] { "Menu", "AllMenus", "Managers" };
-        [SerializeField] private string allMenusScene;
+        [SerializeField] private ScenesDataConfig scenesDataConfig;
+        [Tooltip("Scenes that are on boot")] [SerializeField] private string[] initScenes = new string[] { "Menu", "AllMenus", "Managers" };
         
         private readonly List<SerializableScene> _activeScenes = new List<SerializableScene>();
         
+        /// <summary>
+        /// Loads all init scenes.
+        /// </summary>
         public void InitScenes()
         {
             foreach (var initScene in initScenes)
@@ -24,6 +26,10 @@ namespace Manager
             }
         }
 
+        /// <summary>
+        /// Loads the scene.
+        /// </summary>
+        /// <param name="sceneName">The scene name to load.</param>
         public void LoadScene(string sceneName)
         {
             if (sceneName == "Exit")
@@ -39,6 +45,10 @@ namespace Manager
             AddScene(scene);
         }
 
+        /// <summary>
+        /// Unloads the scene.
+        /// </summary>
+        /// <param name="sceneName">The scene name to unload.</param>
         public void UnloadScene(string aSceneName)
         {
             SerializableScene aScene = scenesDataConfig.GetSceneByName(aSceneName);
@@ -54,12 +64,22 @@ namespace Manager
                 Debug.LogWarning($"{aScene.name} not active!");
             }
         }
-        public void AddScene(SerializableScene scene)
+
+        /// <summary>
+        /// Loads a new scene with scene mode aditive.
+        /// </summary>
+        /// <param name="scene">Serializable scene to load.</param>
+        private void AddScene(SerializableScene scene)
         {
             SceneManager.LoadScene(scene.index, LoadSceneMode.Additive);
             _activeScenes.Add(scene);
         }
 
+        /// <summary>
+        /// Subscribes an event to an Add scene event
+        /// </summary>
+        /// <param name="sceneName">Scene name to load the event to</param>
+        /// <param name="action">Action to subscribe.</param>
         public void SubscribeEventToAddScene(string sceneName, Action action)
         {
             SerializableScene aScene = scenesDataConfig.GetSceneByName(sceneName);
@@ -67,6 +87,11 @@ namespace Manager
             aScene.OnSceneAdded += action;
         }
         
+        /// <summary>
+        /// Unsubscribes an event from the Add scene event
+        /// </summary>
+        /// <param name="sceneName">Scene name to unload the action</param>
+        /// <param name="action">Action to unsubscribe.</param>
         public void UnsubscribeEventToAddScene(string sceneName, Action action)
         {
             SerializableScene aScene = scenesDataConfig.GetSceneByName(sceneName);
