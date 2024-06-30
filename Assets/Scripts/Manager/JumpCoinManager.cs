50386a73abc8a7585afbb6d741b50ab580e4616e
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-06-30 19:12:59 -0300

    Refactor jump coin manager

--- Assets/Scripts/Manager/JumpCoinManager.cs
+++ Assets/Scripts/Manager/JumpCoinManager.cs
@@ -1,40 +1,49 @@
-using PlayerScripts;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Coins.JumpCoin;
-using Manager;
 using UnityEngine;
 
-public class JumpCoinManager : MonoBehaviour
+namespace Manager
 {
-    [SerializeField] private float secondsUntilEnableCoin = 4;
-    [SerializeField] private string coinObtainedEvent = "coinObtained";
-    [SerializeField] private string modifyJumpValuesEvent = "noteModified";
-
-    private void Start()
+    public class JumpCoinManager : MonoBehaviour
     {
-        EventManager.Instance?.SubscribeTo(coinObtainedEvent, OnCoinObtained);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.Instance?.UnsubscribeTo(coinObtainedEvent, OnCoinObtained);
-    }
-
-    private void OnCoinObtained(Dictionary<string, object> message)
-    {
-        GameObject aGameObject = (GameObject)message["gameObject"];
-
-        EventManager.Instance?.TriggerEvent(modifyJumpValuesEvent, new Dictionary<string, object>() { {"value", 1} });
+        [SerializeField] private float secondsUntilEnableCoin = 4;
         
-        StartCoroutine(EnableCoin(aGameObject));
-    }
+        [Header("Events")]
+        [SerializeField] private string coinObtainedEvent = "coinObtained";
+        [SerializeField] private string modifyJumpValuesEvent = "noteModified";
 
-    IEnumerator EnableCoin(GameObject coinObject)
-    {
-        yield return new WaitForSeconds(secondsUntilEnableCoin);
+        private void Start()
+        {
+            EventManager.Instance?.SubscribeTo(coinObtainedEvent, OnCoinObtained);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.Instance?.UnsubscribeTo(coinObtainedEvent, OnCoinObtained);
+        }
+
+        /// <summary>
+        /// Event that executes when coin is obtained.
+        /// </summary>
+        private void OnCoinObtained(Dictionary<string, object> message)
+        {
+            GameObject aGameObject = (GameObject)message["gameObject"];
+
+            EventManager.Instance?.TriggerEvent(modifyJumpValuesEvent, new Dictionary<string, object>() { {"value", 1} });
         
-        coinObject.GetComponent<JumpCoin>().Enable();
+            StartCoroutine(EnableCoin(aGameObject));
+        }
+
+        /// <summary>
+        /// Coroutine that enables the coin after some time.
+        /// </summary>
+        /// <param name="coinObject">jump note to enable again.</param>
+        IEnumerator EnableCoin(GameObject coinObject)
+        {
+            yield return new WaitForSeconds(secondsUntilEnableCoin);
+        
+            coinObject.GetComponent<JumpCoin>().Enable();
+        }
     }
 }
